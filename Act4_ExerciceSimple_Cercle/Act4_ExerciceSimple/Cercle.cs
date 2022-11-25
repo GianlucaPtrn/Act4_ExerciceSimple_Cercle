128a9501b56fecde75375481e9975679f90efb15
GianlucaPtrn <200188@site.asty-moulin.be>
2022-11-25 14:22:14 +0100

    add code POO Cercle

---
+++ Act4_ExerciceSimple_Cercle/Act4_ExerciceSimple/Cercle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Act4_ExerciceSimple
+{
+    class Cercle
+    {
+        private double _rayon;
+
+		public double Rayon
+		{
+			get{ 
+				return _rayon; 
+			}
+			set{
+				_rayon = value; 
+			}
+		}
+
+        //constructeur 
+        public Cercle (double rayon)
+        {
+            _rayon = rayon;
+        }
+
+        public double CalculeAire()
+		{
+			double airResult = Math.PI * _rayon * _rayon;
+            return airResult;
+		}
+
+        public double CalculePerimetre()
+        {
+            double perimetreResult = Math.PI * _rayon * 2;
+            return perimetreResult;
+        }
+
+        public string CaracteristiqueCercle(double airResult, double perimetreResult)
+        {
+            string Caracteristique = "le cercle de rayon :" + " " + _rayon + " " + "a pour périmetre : " + " " + perimetreResult.ToString("f2") + " " + "et une aire de : " + " " + airResult.ToString("f2");
+            return Caracteristique;
+        }
+
+    }
+}
---
+++ Act4_ExerciceSimple_Cercle/Act4_ExerciceSimple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Act4_ExerciceSimple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenu dans ce programme sur le cercle");
+            Console.WriteLine("Tapez un rayon pour votre cercle");
+            double Rayon = int.Parse(Console.ReadLine());
+
+            Cercle cercle = new Cercle(Rayon);
+            double airResult = cercle.CalculeAire();
+            double perimResult = cercle.CalculePerimetre();
+
+            Console.WriteLine(cercle.CaracteristiqueCercle(airResult, perimResult));
+        }
+    }
+}
